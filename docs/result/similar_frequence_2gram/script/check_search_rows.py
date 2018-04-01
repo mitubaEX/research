300ea02ea16b7720c6d445c5c7685e288e1fee3e
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-04-01 16:59:28 +0900

    add jaccard README.md

--- docs/result/similar_frequence_2gram/script/check_search_rows.py
+++ docs/result/similar_frequence_2gram/script/check_search_rows.py
@@ -5,13 +5,14 @@ filename = sys.argv[1]
 each_threshold_rows_count = [0, 0, 0, 0, 0]
 
 with open(filename, 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        if float(row[0]) <= 0.25:
+    reader = list(csv.reader(f))
+    for index, row in enumerate(reader):
+        if float(row[0]) <= 1.0:
             if float(row[1]) >= 0.75:
-                # each_threshold_rows_count[0] += 1
-                print()
+                each_threshold_rows_count[0] += 1
+                # print(index)
             else:
-                # each_threshold_rows_count[1] += 1
+                each_threshold_rows_count[1] += 1
+                # pass
 
-                # print(each_threshold_rows_count)
+print(each_threshold_rows_count)
