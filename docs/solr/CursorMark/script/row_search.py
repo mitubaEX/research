453f528aafa65726e7d3cfeef9f285bf6ed85399
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-04-23 00:37:09 +0900

    CursorMarkをやめてrowsで頑張って行く感じにした

--- docs/solr/CursorMark/script/row_search.py
+++ docs/solr/CursorMark/script/row_search.py
@@ -2,6 +2,7 @@ import requests
 import time
 import sys
 
+
 def convertHexData(row_birthmark):
     hex_row = ""
     for j in row_birthmark:
@@ -13,51 +14,50 @@ def convertHexData(row_birthmark):
         hex_row += ','
     return hex_row
 
-def parsePostData(postData):
-    return convertHexData(postData.split(',', 3)[3])
 
+import os
+os.makedirs('search_result', exist_ok=True)
 postFile = sys.argv[1]
 with open(postFile, 'r') as f:
     import csv
     for row in csv.reader(f):
+        if len(row[3:]) <= int(sys.argv[2]):
+            break
         postData = convertHexData(row[3:])
         print(postData)
 
         payload = {'indent': 'on', 'q': 'encode_data:'+postData, 'sort': 'score desc',
-                'wt': 'json', 'rows': '100', 'cursorMark': '*', 'fl': '*,score'}
+                   'wt': 'json', 'rows': '1000', 'fl': '*,score'}
 
         start = time.time()
         sumQtime = 0
 
         r = requests.get(
             'http://localhost:8983/solr/2gram/select', params=payload)
-        print(r.json())
-        nextCursorMark = r.json()['nextCursorMark']
-        maxScore = r.json()['response']['maxScore']
+        maxScore = float(r.json()['response']['maxScore'])
         print(maxScore)
         print(float(r.json()['response']['docs'][-1]['score']))
-        if float(r.json()['response']['docs'][-1]['score']) < maxScore * (2 / 3):
-            break
-        # print(r.json()['response']['docs'][-1]['score'])
-        beforeCursorMark = ''
-
+        starts = 1000
         while True:
-            payload = {'indent': 'on', 'q': 'encode_data:'+postData, 'sort': 'id desc',
-                       'wt': 'json', 'rows': '100', 'cursorMark': nextCursorMark, 'fl': '*,score'}
-            r = requests.get(
-                'http://localhost:8983/solr/2gram/select', params=payload)
-            nextCursorMark = r.json()['nextCursorMark']
-            # print(r.json()['response']['maxScore'])
+            with open('search_result/'+row[0]+'2gram', 'w') as write_file:
+                write_file.write(','.join(row) + '\n')
+                for result in r.json()['response']['docs']:
+                    if float(result['score']) < maxScore * (2 / 3):
+                        break
+                    write_file.write('{0},{1},{2},{3}\n'.format(
+                        result['filename'], result['score'], result['barthmark'], result['data'].replace('quot;', '')))
             if float(r.json()['response']['docs'][-1]['score']) < maxScore * (2 / 3):
                 break
-            if nextCursorMark == beforeCursorMark:
-                break
-            beforeCursorMark = nextCursorMark
-
+            payload = {'indent': 'on', 'q': 'encode_data:'+postData,
+                       'wt': 'json', 'rows': '1000', 'fl': '*,score', 'start': starts}
+            r = requests.get(
+                'http://localhost:8983/solr/2gram/select', params=payload)
+            starts += 1000
             # qtime
             sumQtime += r.json()['responseHeader']['QTime']
+            print('{0}, {1}'.format(maxScore, float(
+                r.json()['response']['docs'][-1]['score'])))
             print(sumQtime)
-
             elapsed_time = time.time() - start
             print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
 
