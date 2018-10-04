a706b1e2c78692d37296d51ef4465ed32bb74057
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-10-04 23:50:14 +0900

    remove dead code

--- docs/paper_experiment/FP/procedure/row_search.py
+++ docs/paper_experiment/FP/procedure/row_search.py
@@ -18,7 +18,7 @@ def convertHexData(row_birthmark):
 import os
 os.makedirs('search_result', exist_ok=True)
 
-# python3 <filename> postFile length birthmark
+# python3 <output> postFile length birthmark
 postFile = sys.argv[1]
 length = sys.argv[2]
 birthmark = sys.argv[3]
@@ -33,50 +33,34 @@ with open(postFile, 'r') as f:
             postData = convertHexData(row[3:])
 
         if birthmark == 'uc':
-            payload = {'indent': 'on', 'q': 'data:'+postData, 'sort': 'score desc',
-                       'wt': 'json', 'rows': '1000', 'fl': '*,score'}
+            payload = {'query': 'data: ' + postData}
         else:
-            payload = {'indent': 'on', 'q': 'encode_data:'+postData, 'sort': 'score desc',
-                       'wt': 'json', 'rows': '1000', 'fl': '*,score'}
+            payload = {'query': 'encode_data: ' + postData}
 
         start = time.time()
         sumQtime = 0
 
         r = requests.post(
             'http://localhost:8983/solr/' + birthmark +
-            '/query?fl=filename,score,place,barthmark,data&rows=1000&sort=score%20desc&wt=json',
-            json={'query': 'encode_data: ' + postData})
+            '/query?fl=output,score,place,barthmark,data&rows=1000000&sort=score%20desc&wt=json',
+            json=payload)
         # print(r.json())
         maxScore = float(r.json()['response']['maxScore'])
         starts = 1000
-        while True:
-            with open('search_result/'+row[0]+birthmark, 'a') as write_file:
-                write_file.write(','.join(row) + '\n')
-                try:
-                    if len(list(r.json()['response']['docs'])) == 0:
-                        break
-                except:
-                    break
-                for result in r.json()['response']['docs']:
-                    write_file.write('{0},{1},{2},{3}\n'.format(
-                        result['filename'], float(result['score'])/maxScore, result['barthmark'], result['data'].replace('quot;', '')))
+        with open('search_result/'+row[0]+birthmark, 'a') as write_file:
+            write_file.write(','.join(row) + '\n')
+            try:
+                if len(list(r.json()['response']['docs'])) == 0:
+                    sys.exit(1)
+            except:
+                sys.exit(1)
+            for result in r.json()['response']['docs']:
+                write_file.write('{0},{1},{2},{3}\n'.format(
+                    result['output'], float(result['score'])/maxScore, result['barthmark'], result['data'].replace('quot;', '')))
 
-            if birthmark == 'uc':
-                payload = {'indent': 'on', 'q': 'data:'+postData,
-                           'wt': 'json', 'rows': '1000', 'fl': '*,score', 'start': starts}
-            else:
-                payload = {'indent': 'on', 'q': 'encode_data:'+postData,
-                           'wt': 'json', 'rows': '1000', 'fl': '*,score', 'start': starts}
-            r = requests.post(
-                'http://localhost:8983/solr/' + birthmark +
-                '/query?fl=filename,score,place,barthmark,data&rows=1000&sort=score%20desc&wt=json&start=' +
-                str(starts),
-                json={'query': 'encode_data: ' + postData})
-            starts += 1000
-
-            # qtime
-            sumQtime += r.json()['responseHeader']['QTime']
-            elapsed_time = time.time() - start
+        # qtime
+        sumQtime += r.json()['responseHeader']['QTime']
+        elapsed_time = time.time() - start
 
         elapsed_time = time.time() - start
         print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
